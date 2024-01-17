c57d36514a31f2f99915a3e36096a5a87d5a7769
Naim <naimabdul71@gmail.com>
2024-01-17 15:48:59 +0700

    add files tmp

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -4,7 +4,7 @@ import path from "path";
 const Multerupload = (req, res, next) => {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "public");
+      cb(null, "tmp");
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
